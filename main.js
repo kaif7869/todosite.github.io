e1818a942467c4129b8533c6e87b324283ef7f31
MohammadKaif Devalapur <108359361+kaif7869@users.noreply.github.com>
2022-12-18 18:55:28 +0530

    My ToDo Website

---
+++ main.js
@@ -0,0 +1,155 @@
+window.addEventListener('load', () => {
+    todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const nameInput = document.querySelector('#name');
+    const newTodoForm = document.querySelector('#new-todo-form');
+
+    const username = localStorage.getItem('username') || '';
+
+    nameInput.value = username;
+
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+    })
+
+    newTodoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const todo = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.category.value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
+
+        todos.push(todo);
+
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+        e.target.reset();
+
+        DisplayTodos();
+
+    })
+    DisplayTodos();
+})
+
+function DisplayTodos() {
+    const todoList = document.querySelector('#todo-list');
+
+    todoList.innerHTML = '';
+
+    todos.forEach(todo => {
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item')
+
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        input.type = 'checkbox';
+        input.checked = todo.done;
+        span.classList.add('bubble');
+
+        if (todo.category == 'personal') {
+            span.classList.add('personal');
+        } else {
+            span.classList.add('college');
+        }
+
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
+
+        content.innerHTML = `<input type="text" value="${todo.content}"readonly>`;
+        edit.innerHTML = 'Edit';
+        deleteButton.innerHTML = 'Delete';
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(label);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
+
+        todoList.appendChild(todoItem);
+
+        if (todo.done) {
+            todoItem.classList.add('done');
+        }
+
+        input.addEventListener('click', e => {
+            todo.done = e.target.checked;
+            localStorage.setItem('todos', JSON.stringify(todos));
+
+            if (todo.done) {
+                todoItem.classList.add('done')
+
+            } else {
+                todoItem.classList.remove('done');
+            }
+            DisplayTodos();
+
+        })
+
+        edit.addEventListener('click', e => {
+            const input = content.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e => {
+                input.setAttribute('readonly', true);
+                todo.content = e.target.value;
+                localStorage.setItem('todos', JSON.stringify(todos))
+                DisplayTodos();
+            })
+        })
+
+        deleteButton.addEventListener('click', e => {
+            todos = todos.filter(t => t != todo);
+            localStorage.setItem('todos', JSON.stringify(todos));
+            DisplayTodos();
+        })
+
+    })
+}
+
+// dropdown
+const dropdown = document.querySelectorAll('.dropdown');
+
+dropdown.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+
+        caret.classList.toggle('caret-rotate');
+
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(Option => {
+        options.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+
+            select.classList.remove('select-clicked');
+
+            caret.classList.remove('caret-rotate');
+
+            menu.classList.remove('menu-open');
+
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        })
+    })
+})
